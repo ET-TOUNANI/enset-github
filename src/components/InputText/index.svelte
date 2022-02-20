f5b58089a3619bc233672b16538a0ebd76b29c2e
akramksb <akram.kssiba@gmail.com>
2022-02-20 20:19:20 +0100

    feat : change input to button

---
+++ src/components/InputText/index.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let value;
+    export let placeholder;
+    export let icon;
+  </script>
+  
+  <div>
+    <span>{icon}</span>
+    <input bind:value {placeholder} />
+  </div>
